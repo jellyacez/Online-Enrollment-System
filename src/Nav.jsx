319678f5763af4fe9161340665f5d5756fa556b7
Astaire <louiedwigth01@gmail.com>
2026-04-09 23:15:47 +0800

    tested a navigation component

---
+++ src/Nav.jsx
@@ -0,0 +1,15 @@
+function Navigation(){
+    return(
+        <nav>
+        <div id="logo"></div>
+        <div id="links">
+            <a href="">links</a>
+            <a href="">links</a>
+            <a href="">links</a>
+            <a href="">links</a>
+            <a href="">links</a>
+        </div>
+        </nav>
+    )
+}
+export default Navigation;
