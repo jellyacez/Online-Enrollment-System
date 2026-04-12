bf143fead0721078f5c1fbd442364405529f90c8
nolifesean <deguzmanseanluigi0506@gmail.com>
2026-04-12 16:44:15 +0800

    new ccs file for student management + renamed component folder to components + added route for student management

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import Routing from './App.jsx';
-import Navigation from './component/Nav.jsx';
-import './css/index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import Routing from "./App.jsx";
+import Navigation from "./components/Nav.jsx";
+import "./css/index.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>  {/*This is necessary to route */}
+    <BrowserRouter>
+      {" "}
+      {/*This is necessary to route */}
       <Navigation />
       <Routing />
     </BrowserRouter>
-  </StrictMode>
-)
+  </StrictMode>,
+);
