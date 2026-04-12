3138547d9c70ee45ea8e5e1efbd585ae973a65bb
Astaire <louiedwigth01@gmail.com>
2026-04-12 17:49:22 +0800

    nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,33 @@
-import { Link } from "react-router-dom"; //this is react "Link", must be used instead of 'link'
-function Navigation() {
-  return (
-    <nav>
-      <div id="logo"></div>
-      <div id="links">
-        <Link to="/">Home</Link>
-        <Link to="/payment">Payment</Link>
-        <Link to="/student-management">Student Management</Link>
-      </div>
-    </nav>
-  );
+import { Link } from 'react-router-dom'; //this is react "Link", must be used instead of 'link'
+import "../css/index.css";
+function Navigation(){
+    return(
+<aside className='sidebar-left'>
+            <div className="sidebar-inner">
+                <div id="logo">
+                    <h2>Header</h2>
+                </div>
+                <div id="links">
+                    <div className="link-container">
+                        <Link to="/">Home</Link>
+                    </div>
+                    <div className="link-container">
+                        <Link to="/student-management">Student Management</Link>
+                    </div>
+                    <div className="link-container">
+                        <Link to="/payment">Payment</Link>
+                    </div>
+                    <div className="link-container">
+                        <Link to="/about">About</Link>
+                    </div>
+
+                </div>
+
+
+            </div>
+        </aside>
+    )
 }
 export default Navigation;
+
+
