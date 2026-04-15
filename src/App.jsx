02ca853d8493b0576be566c3a0c46029a223c117
nolifesean <deguzmanseanluigi0506@gmail.com>
2026-04-15 22:32:45 +0800

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,17 @@ import ManageSubjects from "./pages/ManageSubjects";
 import Login from "./pages/login";
 
 export default function Routing() {
-
   const location = useLocation();
-  const isLoginPage = location.pathname.toLowerCase().includes('login');
+  const isLoginPage = location.pathname.toLowerCase().includes("login");
   return (
     <>
-    {!isLoginPage && <Navbar />}
-    <Routes>
-      <Route path="/" element={<Homepage />} />
-      <Route path="/payment" element={<Payment />} />
-      <Route path="/student-management" element={<ManageSubjects />} />
-      <Route path="/login" element={<Login/>} />
-    </Routes>
+      {!isLoginPage}
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/payment" element={<Payment />} />
+        <Route path="/student-management" element={<ManageSubjects />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
   );
 }
