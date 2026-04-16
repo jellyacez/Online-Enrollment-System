a5fa05b53f13ce507a95983b013460f39f5e5aac
Astaire <louiedwigth01@gmail.com>
2026-04-16 11:29:02 +0800

    removed facebook login button

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../css/login-signup.css";
-import Homepage from "./Homepage";
 import Logo from "../assets/UniLogo.png";
+import "../css/login-signup.css";
 
 export default function Login() {
   const navigate = useNavigate();
@@ -149,21 +148,23 @@ export default function Login() {
             </svg>
             Continue with Google
           </button>
-          <button
-            type="button"
-            className="social-btn facebook-btn"
-            onClick={handleFacebookLogin}
-          >
-            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.469h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.469h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"
-                fill="#ffffff"
-              />
-            </svg>
-            Continue with Facebook
-          </button>
+         
         </div>
       </div>
     </div>
   );
 }
+//  <button
+//             type="button"
+//             className="social-btn facebook-btn"
+//             onClick={handleFacebookLogin}
+//           >
+//             <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+//               <path
+//                 d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.469h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.469h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"
+//                 fill="#ffffff"
+//               />
+//             </svg>
+//             Continue with Facebook
+//           </button>
+// Baka mag tampo si kevin, inalis yung facebook nya ·. °՞(ᗒ□ᗕ)՞°·. · 
